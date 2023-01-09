1e5e1eb59010bd7812d2a3484f58bdf4c1d909e3
Mara Green <maragreen42@gmail.com>
2023-01-08 21:43:52 -0800

    add AssSongs conponent file + continue work on SearchBar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBar from './Components/NavBar/navBar';
 import MusicTable from './Components/MusicTable/musicTable';
+import SearchBar from './Components/SearchBar/searchBar';
+
+
 function App() {
 
   const [songs, setSongs] = useState([]);
-
+  const [search, setSearch] =useState([])
 
   useEffect( () => {
     getAllSongs()
@@ -22,8 +25,9 @@ async function getAllSongs(){
   return (
     <div >
       <NavBar/>
+      <SearchBar/>
       <MusicTable parentSongs={songs}/>
-      <button onClick={() => getAllSongs()}>Get All Songs</button>
+      {/* <button onClick={() => getAllSongs()}>Get All Songs</button> */}
     </div>
   );
 }
--- src/Components/SearchBar/searchBar.jsx
+++ src/Components/SearchBar/searchBar.jsx
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
+import MusicTable from '../MusicTable/musicTable';
+import axios from 'axios';
+
 
 
 const SearchBar = (props) => {
+
+    
+
+
+    
     return ( 
+
+        <div>
+            <div  className = 'search-bar'>
+            <input 
+                type='search' 
+                placeholder= 'Search' 
+            />
+            </div>
+            <div className="search-button">
+                <button>Search</button>
+            </div>
+        </div>
 
         
      );
-}
+};
  
 export default SearchBar;
