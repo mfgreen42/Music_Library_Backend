384e8bb489207c37374fd66a1dae3705a612db6d
Mara Green <maragreen42@gmail.com>
2023-01-17 16:53:24 -0800

    save point

--- src/Components/SearchBar/searchBar.jsx
+++ src/Components/SearchBar/searchBar.jsx
@@ -1,46 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import MusicTable from '../MusicTable/musicTable';
+import React, { useEffect, useState } from 'react';
 
+const SearchBar = ({event}) => {
 
-const SearchBar = (props) => {
-
-    const [allResults, setAllResults] =useState([]);
-    const [filteredData, setFilteredData] = useState(allResults);
-
-    const handleChange = (props) => {
-
-    }
-
-    useEffect(() => {
-        axios('http://127.0.0.1:8000/api/music/')
-        .then(response => {
-        console.log(response.data)
-        setAllResults(response.data);
-        setFilteredData(response.data);
-        })
-        .catch(error => {
-        console.log('Error getting fake data: '+error);
-        })
-        }, []); 
-        
-    const handleSearch = (event) => {
-        let value = event.target.value.toLowerCase();
-        let result = [];
-        console.log(value);
-        result = allResults.filter((data) => {
-        return data.title.search(value) !== -1;
-        });
-        setFilteredData(result);
-        }
     
     return ( 
         <div className = 'search-bar'>
             <label>Search:</label>
-            <input type='text' onChange={(event) => handleSearch(event)} />
+            <input type='text' onChange={(event) => onChange(event.target.value)} />
         </div>
         
      );
 };
  
 export default SearchBar;
+
+
